2fd2c706017479326f1094c144298522f2f83b91
Thomsen96 <jonastoftethomsen@gmail.com>
2022-02-26 10:19:33 +0100

    Add docker-compose and use of Letsencrypt
    Also moved the the applications server setup to its own folder. Since we might at somepoint add more/ other services

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
-
 # https://serverfault.com/questions/638600/auto-accept-rsa-key-fingerprint-from-command-line
 # -o "StrictHostKeyChecking no" 
-scp -i $SSH_KEY_NAME -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" ./init.sh root@thomsen-it.dk:/root/init.sh
+scp -r -i $SSH_KEY_NAME -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" ./ApplicationServer/ root@thomsen-it.dk:/root/
 
-ssh -i $SSH_KEY_NAME -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" root@thomsen-it.dk "chmod +x ./init.sh; ./init.sh"
+ssh -i $SSH_KEY_NAME -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" root@thomsen-it.dk "cd ApplicationServer; chmod +x ./init.sh; ./init.sh"
 
 echo "Setup complete."
