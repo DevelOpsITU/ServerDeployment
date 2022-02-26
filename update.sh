9e7d2d9e3fe81ce61e134cedceed4605ccbf660f
Thomsen96 <jonastoftethomsen@gmail.com>
2022-02-26 17:25:15 +0100

    start an application server and deploy a minitwit container
    Creating the server and running the run command correctly pulls the latest minitwit docker and starts it behind nginx that have TLS

--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # https://serverfault.com/questions/638600/auto-accept-rsa-key-fingerprint-from-command-line
 # -o "StrictHostKeyChecking no" 
 scp -r -i $SSH_KEY_NAME -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" ./ApplicationServer/ root@thomsen-it.dk:/root/
